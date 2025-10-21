6aa3719731180bcabfa2af1ee57e56df9d59ee8f
s1mplist <gustavopinheiro.az@outlook.com>
2025-10-20 21:01:38 -0300

    feat: added entrypoint for tests

---
+++ src/api/webhook.py
@@ -0,0 +1,11 @@
+import logging
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+logger = logging.getLogger(__name__)
+app = FastAPI()
+
+
+@app.post("/webhook")
+async def notion_webhook(request: Request):
+    return JSONResponse(content={"teste": "ok"}, status=200)
