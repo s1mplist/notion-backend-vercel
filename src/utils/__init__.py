a67c60812fb147d21ac6743701ef3924f3f776ea
s1mplist <gustavopinheiro.az@outlook.com>
2025-10-27 12:50:59 -0300

    feat: added utils methods

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,19 @@
-# Utils package
+"""Utilities package for common helper functions."""
+
+from .json_utils import json_default_handler, to_json_string
+from .notion_utils import normalize_prop_name, extract_text, extract_date
+from .logging_config import setup_logging, get_logger
+from .html_utils import inline_assets, inline_css, inline_local_images
+
+__all__ = [
+    "json_default_handler",
+    "to_json_string",
+    "normalize_prop_name",
+    "extract_text",
+    "extract_date",
+    "setup_logging",
+    "get_logger",
+    "inline_assets",
+    "inline_css",
+    "inline_local_images",
+]
